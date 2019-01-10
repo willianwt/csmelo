d214437ae85265730dc715bcc5fbce4f83700748
Willian <willianwt@gmail.com>
2019-01-10 01:16:17 -0200

    algumas melhorias no codigo

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
-    $user = "csmelo";
-    $pass = "3Saucuu0GrigE372";
-    $db = "csmelo";
+    $user = "id8001784_csmelocutelaria";
+    $pass = "csmelo2018";
+    $db = "id8001784_csmelo";
 
     $conexao = new mysqli($host,$user,$pass,$db) or die($conexao -> error);
     if (!mysqli_set_charset($conexao, 'utf8')) {
